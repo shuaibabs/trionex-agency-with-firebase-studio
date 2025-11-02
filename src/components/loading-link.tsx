1e7249bcbc1cbb2bec880dcd5f3161d5ba97d675
Shuaib <mhshuaibabbasi@gmail.com>
2025-11-02 10:03:33 +0000

    why spinner not working

--- src/components/loading-link.tsx
+++ src/components/loading-link.tsx
@@ -1,8 +1,8 @@
-
 'use client';
 
 import Link, { type LinkProps } from 'next/link';
 import React from 'react';
+import { useLoading } from '@/hooks/use-loading-store';
 
 type LoadingLinkProps = LinkProps & {
   children: React.ReactNode;
@@ -11,11 +11,20 @@ type LoadingLinkProps = LinkProps & {
   'aria-label'?: string;
 };
 
-// This component is now a simple wrapper around Next.js's Link.
-// The loading state is handled globally by NavigationEvents.
-export default function LoadingLink({ children, ...props }: LoadingLinkProps) {
+// This component wraps Next.js's Link to trigger the global loading
+// bar on click, providing immediate feedback to the user.
+export default function LoadingLink({ children, onClick, ...props }: LoadingLinkProps) {
+  const { startLoading } = useLoading();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    startLoading();
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <Link {...props}>
+    <Link {...props} onClick={handleClick}>
       {children}
     </Link>
   );
