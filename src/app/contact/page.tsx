74eaf76d152cde2e019dd1cb0f5418b7fc1b5516
Shuaib <mhshuaibabbasi@gmail.com>
2025-11-03 18:19:01 +0000

    keep map as it as without any css or dark mode

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -137,7 +137,7 @@ export default function ContactPage() {
             </div>
              <div className="h-96 w-full rounded-lg overflow-hidden border">
                 <iframe
-                    className="h-full w-full map-dark-mode"
+                    className="h-full w-full"
                     src="https://maps.google.com/maps?q=Jalalabad,%20Shamli,%20Uttar%20Pradesh,%20247772&t=&z=13&ie=UTF8&iwloc=&output=embed"
                     frameBorder="0"
                     scrolling="no"
